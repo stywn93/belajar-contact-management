9b8fb6aa010faf3013f7093092ae951e8262231c
Iwan Bagus Setyawan <stywn93@gmail.com>
2025-09-24 21:20:19 +0700

    First Commit

---
+++ src/lib/api/UserApi.js
@@ -0,0 +1,12 @@
+export const userRegister = async ({username, password, name}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            username, password, name
+        })
+    })
+}
