8c041eced687e30686e2d349e01db9338baeadea
Iwan Bagus Setyawan <stywn93@gmail.com>
2025-09-27 03:56:22 +0700

    otw contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "./components/Layout.jsx";
 import UserRegister from "./components/User/UserRegister.jsx";
@@ -7,23 +7,27 @@ import UserLogin from "./components/User/UserLogin.jsx";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 import UserProfile from "./components/User/UserProfile.jsx";
 import UserLogout from "./components/User/UserLogout.jsx";
+import ContactCreate from "./components/Contact/ContactCreate.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route element={<Layout/>}>
-                  <Route path={"/register"} element={<UserRegister/>}/>
-                  <Route path={"/login"} element={<UserLogin/>}/>
-              </Route>
-              <Route path={"/dashboard"} element={<DashboardLayout/>}>
-                  <Route path={"contacts"} element={<div>Contacts</div>}/>
-                  <Route path={"users"}>
-                      <Route path={"profile"} element={<UserProfile/>}/>
-                      <Route path={"logout"} element={<UserLogout/>}/>
-                  </Route>
-              </Route>
-          </Routes>
-      </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout/>}>
+                    <Route path={"/register"} element={<UserRegister/>}/>
+                    <Route path={"/login"} element={<UserLogin/>}/>
+                </Route>
+                <Route path={"/dashboard"} element={<DashboardLayout/>}>
+                    <Route path={"users"}>
+                        <Route path={"profile"} element={<UserProfile/>}/>
+                        <Route path={"logout"} element={<UserLogout/>}/>
+                    </Route>
+                    <Route path={"contacts"}>
+                        <Route index element={<div>Contacts</div>}/>
+                        <Route path={"create"} element={<ContactCreate/>}/>
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 )
